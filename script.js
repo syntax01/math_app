a59079e76909bf89b03eda8bb5434952efd5d0a2
computer_chris <computergeek99@gmail.com>
2024-06-28 00:01:06 -0700

    app store link and working image navigation

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 function processImageNavigation(evt) {
-    if(evt.target.classList.contains('next')) {
-        console.log('next');
+
+    var isDirectionNext = evt.target.classList.contains('next');
+    var currentImageSrc = elImageImage.getAttribute('src');
+    var currentImageId = Number(currentImageSrc.replace('./img/iphone_pro_max_','').replace('.png',''));
+    var newImageId = 1;
+
+    if(isDirectionNext) {
+        if(currentImageId == 4) {
+            newImageId = 1;
+        } else {
+            newImageId = ++currentImageId;
+        }
     } else {
-        console.log('prev');
+        if(currentImageId == 1) {
+            newImageId = 4;
+        } else {
+            newImageId = --currentImageId;
+        }
     }
+    var newImageSrc = './img/iphone_pro_max_' + newImageId + '.png';
+    elImageImage.setAttribute('src', newImageSrc);
+
+
+
 }
 
 let clickers = document.querySelectorAll('.content-2 .clicker');
+let elImageImage = document.querySelector('#image_image');
+let elImageText = document.querySelector('#image_text');
 //clickers.forEach(el => el.addEventListener('click', processImageNavigation));
 clickers.forEach(el => {
     el.addEventListener('click', evt => {
